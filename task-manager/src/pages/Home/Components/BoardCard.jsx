8f023fc9d705d5e53dbae78a5f0363384b913c4e
ayaamazloum <ayamazloum.a@gmail.com>
2024-04-13 20:49:00 +0300

    Created boards list page

---
+++ task-manager/src/pages/Home/Components/BoardCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoardCard = () => {
+  return (
+      <div className='border small-card-padding rounded pointer'>
+        Board Title
+    </div>
+  )
+}
+
+export default BoardCard
--- task-manager/src/pages/Home/index.jsx
+++ task-manager/src/pages/Home/index.jsx
@@ -1,9 +1,17 @@
+import './style.css'
 import Logout from "../../Components/Logout"
+import BoardCard from "./Components/BoardCard"
 
 const Home = ({ handleUserLogged }) => {
   return (
-    <div>
+    <div className="page home center flex column center gap-50">
       <Logout handleUserLogged={handleUserLogged} />
+      <h1>Boards</h1>
+      <div className="flex row center wrap gap-30">
+        <BoardCard />
+        <BoardCard />
+        <BoardCard />
+      </div>
     </div>
   )
 }
