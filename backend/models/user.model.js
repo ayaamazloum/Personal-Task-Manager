cc9549d23d9b8dea78c826127a8844a67699752c
ayaamazloum <ayamazloum.a@gmail.com>
2024-04-13 13:22:04 +0300

    Added all needed schemas

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: "Title is required" },
+    description: { type: String, required: "Description is required" },
+    attachments: [{ type: String }],
+    tags: [{ type: String }]
+});
+
+const columnSchema = ({
+    title: { type: String, required: "Title is required" },
+    tasks: [taskSchema]
+});
+
+const boardSchema = new mongoose.Schema({
+    title: { type: String, required: "Title is required" },
+    columns: [columnSchema]
+});
+
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: "First name is required"
-    },
-    lastName: {
-        type: String,
-        required: "Last name is required"
-    },
-    username: {
-        type: String,
-        required: "Username is required",
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: "Password is required",
-    },
+    firstName: { type: String, required: "First name is required" },
+    lastName: { type: String, required: "Last name is required" },
+    username: { type: String, required: "Username is required", unique: true },
+    password: { type: String, required: "Password is required" },
+    boards: [boardSchema]
 });
 
 userSchema.pre("save", async function (next) {
